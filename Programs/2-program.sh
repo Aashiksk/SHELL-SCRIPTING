14d2bd111306f12b3e067f41e6a429127f074d35
Aashik <your_github_email@example.com>
2026-04-11 17:33:18 +0530

    shell scripting

--- Programs/2-program.sh
+++ Programs/2-program.sh
@@ -6,13 +6,14 @@ MYID=$(id -u)
 if [ "$MYID" -ne 0 ]
 then
   echo "You are not root user"  
-else
-  sudo yum install python3 -y
-  if [ $? -eq 0 ]
-  then 
-      echo "python3 is installed successfully"
-    else
-      echo "python3 installation failed"
-    fi
-
+  exit 1
 fi
+
+dnf list installed nginx
+if [ $? -ne 0 ]
+then 
+    echo "nginx is not installed, installing nginx"
+else
+    echo "nginx is already installed"
+fi
+
